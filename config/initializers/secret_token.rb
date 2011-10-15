071293bcd3a2346393909a30990e8783da8196e7
John Dell <spovich@gmail.com>
2011-10-15 14:36:45 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taganoid::Application.config.secret_token = 'b7bee9318bacb641c01166ef0da0f0b8202f4a3292c242d10bdb0809753ebd5f097c8a4e671b68ca8e00db7f35d45be7496b47b11bcdb653effce29611093d52'
