d478a98a91a52dec37ce16db6cb9d60cd1b222b0
weshays <weshays@gbdev.com>
2011-10-15 19:57:05 -0700

    Finished, but not tested, the settings models and controllers.

--- app/helpers/setting/entry_types_helper.rb
+++ app/helpers/setting/entry_types_helper.rb
@@ -1,2 +1,10 @@
 module Setting::EntryTypesHelper
+  
+  def delete_entry_type(entry_type)
+    link_to('Delete', 
+            setting_entry_type_path(entry_type[:id]), 
+            {:method => :delete,
+             :confirm => "Are you sure you want to delete #{entry_type[:name]}?"})
+  end
+  
 end
--- app/helpers/setting/groups_helper.rb
+++ app/helpers/setting/groups_helper.rb
@@ -1,2 +1,10 @@
 module Setting::GroupsHelper
+  
+  def delete_group(group)
+    link_to('Delete', 
+            setting_group_path(group[:id]), 
+            {:method => :delete,
+             :confirm => "Are you sure you want to delete #{group[:name]}?"})
+  end
+  
 end
