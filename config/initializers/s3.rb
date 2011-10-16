a80ed794acf853fe93076b12f552784de749932f
duonoid <duonoid@wippoc.com>
2011-10-16 01:13:49 -0700

    fix: don't blow up when S3 credentials aren't in environment

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,4 @@
 AWS::S3::Base.establish_connection!(
-    :access_key_id     => ENV['S3_KEY'],
-    :secret_access_key => ENV['S3_SECRET']
+    :access_key_id     => (ENV['S3_KEY'] || 'foo'),
+    :secret_access_key => (ENV['S3_SECRET'] || 'bar')
 )
