b8475b6a0e8bc256d873df70f38ca4596c972e7f
duonoid <duonoid@wippoc.com>
2011-10-15 23:40:59 -0700

    refactor: whitespace

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,27 +2,28 @@ class Entry < ActiveRecord::Base
   belongs_to :entry_type
   belongs_to :group
   belongs_to :status
-  
+
   scope :between_dates, lambda { |start_date, end_date|
-    where('created_at BETWEEN ? AND ?', 
-          Date.parse(start_date).beginning_of_day, 
+    where('created_at BETWEEN ? AND ?',
+          Date.parse(start_date).beginning_of_day,
           Date.parse(end_date).end_of_day)
   }
-  
-  
-  
-  validates_presence_of :entry_type_id, 
+
+
+
+
+  validates_presence_of :entry_type_id,
                         :message => 'Select entry type'
-                        
-  validates_presence_of :group_id, 
+
+  validates_presence_of :group_id,
                         :message => 'Select group'
-                        
-  validates_presence_of :status_id, 
-                        :message => 'Select status'                                                
+
+  validates_presence_of :status_id,
+                        :message => 'Select status'
 
 
-  validates_length_of :title, 
+  validates_length_of :title,
                       :within => 1..100,
-                      :too_short => '^Title is required', 
+                      :too_short => '^Title is required',
                       :too_long => 'Title must be 40 characters or less'
 end
