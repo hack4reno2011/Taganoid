c42332c844578938657286c347f3d84883ddae01
weshays <weshays@gbdev.com>
2011-10-24 18:35:06 -0700

    Commented out auth for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate
+  #before_filter :authenticate
 
 protected
   def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "hack4reno"
-    end
+    # authenticate_or_request_with_http_basic do |username, password|
+    #   username == "admin" && password == "hack4reno"
+    # end
   end  
 end
